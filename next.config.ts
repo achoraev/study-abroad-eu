adb41503c3e014ecc334e95865e2f78887f1b64e
Angel Raev <angel.raev@sap.com>
2026-04-18 13:41:25 +0300

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'studyabroadeu.net' },
